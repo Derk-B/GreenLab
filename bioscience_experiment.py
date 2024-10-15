1046800f4f115ce5c5bd12cde5b693a7553f341f
Derk Blom <74244634+Derk-B@users.noreply.github.com>
2024-10-15 17:17:23 +0200

    asdf

--- bioscience_experiment.py
+++ bioscience_experiment.py
@@ -1,69 +1,57 @@
-import numpy as np
 import bioscience as bs
 import gc
-import pandas as pd
-from sklearn.datasets import make_biclusters
 import sys
+import pandas as pd
 
-def calculate_sparsity(array):
-    # Convert the input to a NumPy array if it's not already
-    array = np.asarray(array)
+class BioScienceExperiment:
+    def __init__(self, dataset_path: str, mode: int):
+        self.dataset_path = dataset_path
+        self.mode = mode
+        self.dataset = None
+        self.finished = False
     
-    # Get the total number of elements
-    total_elements = array.size
-    
-    # Count the number of non-zero elements
-    non_zero_elements = np.count_nonzero(array)
-    
-    # Calculate sparsity
-    sparsity = (total_elements - non_zero_elements) / total_elements
-    
-    return sparsity
+    def load(self):
+        data = pd.read_csv(self.dataset_path, sep=',').values
+        lengths = None
+        geneNames = None
+        colNames = [str(i) for i in range(len(data[0]))]
+        
+        self.dataset = bs.Dataset(data, geneNames=geneNames, columnsNames=colNames, lengths=lengths)
+        # self.dataset = bs.load(path=self.dataset_path, index_gene=0, naFilter=False, head=0)
 
-if len(sys.argv) < 3:
-    print("Usage: python bioscience_experiment.py <dataset_path> <mode>")
-    sys.exit(1)
+    def run(self):
+        if self.dataset is None:
+            self.load()
+            
+        # bs.tpm(self.dataset)
+        # bs.binarize(self.dataset)
+        
+        listModels = bs.bibit(self.dataset, cMnr=2, cMnc=2, mode=self.mode, debug=True)
+        
+        self.finished = True
+        
+        del self.dataset
+        del listModels
+        self.dataset = None
+        gc.collect()
 
-dataset_path = sys.argv[1]
-mode = int(sys.argv[2])
+    def is_finished(self) -> bool:
+        """Check if the experiment has finished."""
+        return self.finished
 
-# 1000 bij 100, duurt ~30 seconden op cpu (single core)
-# 2400 bij 100, duurt ~1 seconde op gpu
-# 2500 bij 100, duurt ineens oneindig op gpu, vreemd wel.
-data, rows, cols = make_biclusters(shape=(2000, 1200), n_clusters=3, random_state=42)
+def main():
+    print("ARGS: ", sys.argv)
+    if len(sys.argv) < 3:
+        print("Usage: python bioscience_experiment.py <dataset_path> <mode>")
+        sys.exit(1)
 
-# Change the 'rate' to add create more missing values, increasing the sparsity
-# data = add_random_missing(data, 0.1)
+    dataset_path = sys.argv[1]
+    mode = int(sys.argv[2])
+    print(dataset_path, mode)
+    # mode = 3
+    experiment = BioScienceExperiment(dataset_path, mode)
+    experiment.load()
+    experiment.run()
 
-# Comment this line to use new data
-data = pd.read_csv(dataset_path, sep=',').values
-
-# Zet de numpy array die sklearn geeft om in een dataset van BioScience.
-lengths = None
-geneNames = None
-colNames = [str(i) for i in range(len(cols[0]))]
-
-dataset_GPU = bs.Dataset(data, geneNames=geneNames, columnsNames=colNames, lengths=lengths)
-dataset_CPU = bs.Dataset(data, geneNames=geneNames, columnsNames=colNames, lengths=lengths)
-
-# Calculate sparsity
-# Use this value to reason about sparsity difference between datasets.
-sparsity = calculate_sparsity(dataset_GPU.data)
-print(f"Sparsity: {sparsity:.4f}")
-
-# RNA-Seq preprocessing
-bs.tpm(dataset_GPU)
-
-# Binary preprocessing
-bs.binarize(dataset_GPU)
-
-# Data mining phase
-if mode == 3:
-    listModels = bs.bibit(dataset_GPU, cMnr=2, cMnc=2, mode=3, deviceCount=1, debug = True)
-else:
-    listModels = bs.bibit(dataset_GPU, cMnr=2, cMnc=2, mode=mode, debug = True)
-
-del listModels
-del dataset_GPU
-gc.collect()
-
+if __name__ == "__main__":
+    main()
